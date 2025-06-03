e9a29c07352f189baa2d22fe64e6c9333e7ebe82
rizky herdiansyah <141700940+rizkyher@users.noreply.github.com>
2025-06-03 08:59:24 +0700

    update nav

--- js/navbar.js
+++ js/navbar.js
@@ -76,3 +76,11 @@
                 });
             }
         });
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("mobile-menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuButton.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  });
